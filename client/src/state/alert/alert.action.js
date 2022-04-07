fe14b305af97ed25b07726c1fe5eec40326db7ad
liuyang <liuyangyangyang1992@gmail.com>
2022-04-07 13:22:35 +1000

    feat:add alert functionr

--- client/src/state/alert/alert.action.js
+++ client/src/state/alert/alert.action.js
@@ -1,13 +1,14 @@
 import { v4 as uuidv4 } from 'uuid';
-
+// command + { 向左缩进
+// command +  } 向右缩进
 const id = uuidv4()
 export const setAlert = (msg, alertType) => ({
   type: 'SET_ALERT',
-  data: {id, msg, alertType}
+  data: { id, msg, alertType }
 })
 
 export const removeAlert = () => ({
-  type:'REMOVE_ALERT',
+  type: 'REMOVE_ALERT',
   data: id
 })
 
